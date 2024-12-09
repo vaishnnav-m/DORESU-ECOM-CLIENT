86cd85fa8b55a99ced616f9e3e479049eb45337e
Vaishnnav M <vaishnnav58@gmail.com>
2024-12-09 09:19:45 +0530

    updated code

--- src/services/adminApi.js
+++ src/services/adminApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setAdminCredentials } from "../store/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3000/api/admin",
+  baseUrl: "https://doresu.store/api/admin",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const adminToken = getState().auth.adminToken;
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../store/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3000",
+  baseUrl: "https://doresu.store",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
